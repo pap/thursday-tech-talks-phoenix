a60116a8a61128dc6a6bbe957769bf8439b22052
Paulo Alves Pereira <odesassossegado@gmail.com>
2020-06-17 14:35:44 +0100

    Enable os_mon
    Enable os_mon erlang application so that we can see OS metrics in the live
    view dashboard.

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule ToDo.MixProject do
   def application do
     [
       mod: {ToDo.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
