d295bf7421079f66acb041a4db458695acc17e9b
Paulo Alves Pereira <odesassossegado@gmail.com>
2020-06-18 13:15:43 +0100

    Create model and add pubsub

--- lib/todo_web/live/components/task_card_component.ex
+++ lib/todo_web/live/components/task_card_component.ex
@@ -8,15 +8,18 @@ defmodule TaskCardComponent do
   def render(assigns) do
     ~L"""
     <div class="card">
-      <header class="card-header">
-        <p class="card-header-title is-size-7"><%= assigns.task.title %></p>
-        <p class="card-header-icon is-size-7 has-text-grey"><%= assigns.task.created_at %></p>
-        <%= if assigns.task.completed do %>
-          ...
-        <% else %>
-          ...
-        <% end %>
-      </header>
+      <div class="card-content">
+        <div class="title is-size-6"><%= assigns.task.title %></div>
+      </div>
+      <footer class="card-footer">
+      <p class="card-footer-item">
+      <%= assigns.task.inserted_at %>
+      </p>
+      <p class="card-footer-item">
+        <span class="button is-small" phx-click="complete" phx-value-task-id="<%= assigns.task.id %>">
+          Mark as Done
+        </span>
+      </p>
     </div>
     """
   end
--- lib/todo_web/live/page_live.ex
+++ lib/todo_web/live/page_live.ex
@@ -1,14 +1,34 @@
 defmodule ToDoWeb.PageLive do
   use ToDoWeb, :live_view
 
+  @topic "tasks"
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", tasks: [])}
+    ToDoWeb.Endpoint.subscribe(@topic)
+    {:ok, assign(socket, query: "", tasks: tasks())}
   end
 
   @impl true
   def handle_event("create", %{"title" => title}, socket) do
-    IO.puts(title)
-    {:noreply, assign(socket, tasks: [])}
+    ToDo.Tasks.create_task(%{title: title})
+    ToDoWeb.Endpoint.broadcast_from!(self(), @topic, "created", %{})
+    {:noreply, assign(socket, tasks: tasks())}
+  end
+
+  def handle_event("complete", %{"task-id" => task_id}, socket) do
+    task = ToDo.Tasks.get_task!(task_id)
+    ToDo.Tasks.update_task(task, %{completed: true})
+    ToDoWeb.Endpoint.broadcast_from!(self(), @topic, "completed", %{})
+    {:noreply, assign(socket, tasks: tasks())}
+  end
+
+  @impl true
+  def handle_info(%{event: _event}, socket) do
+    {:noreply, assign(socket, tasks: tasks())}
+  end
+
+  defp tasks do
+    ToDo.Tasks.list_tasks([{:completed, false}])
   end
 end
